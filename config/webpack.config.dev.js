e69ae3db4b6d725622cd638ff415f53a3e21bfe9
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-24 16:48:11 -0600

    Add module loader and import mocha and chai for testing

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -79,7 +79,7 @@ module.exports = {
       'react-native': 'react-native-web'
     }
   },
-  
+
   module: {
     // First, run the linter.
     // It's important to do this before Babel processes the JS.
@@ -111,7 +111,8 @@ module.exports = {
           /\.(js|jsx)$/,
           /\.css$/,
           /\.json$/,
-          /\.svg$/
+          /\.svg$/,
+          /node_modules/
         ],
         loader: 'url',
         query: {
@@ -125,7 +126,7 @@ module.exports = {
         include: paths.appSrc,
         loader: 'babel',
         query: {
-          
+
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
@@ -157,7 +158,11 @@ module.exports = {
       }
     ]
   },
-  
+
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+
   // We use PostCSS for autoprefixing only.
   postcss: function() {
     return [
