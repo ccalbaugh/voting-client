a4edc923b5e022027c34f9f2cff8c425d76cd244
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-25 10:28:10 -0600

    Add test suite and onClick for Voting component

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -5,14 +5,13 @@ export default React.createClass({
 		return this.props.pair || [];
 	},
 	render: function() {
-		return (
-			<div className="voting">
-				{this.getPair().map(entry => {
-					<button key={entry}>
-						<h1>{entry}</h1>
-					</button>
-				})}
-			</div>;
-		);
+		return <div className="voting">
+			{this.getPair().map(entry =>
+				<button key={entry}
+						onclick={() => this.props.vote(entry)}>
+					<h1>{entry}</h1>
+				</button>
+			)}
+		</div>;
 	}
 });
---
+++ test/components/Voting_spec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  Simulate
+} from 'react-addons-test-utils';
+import Voting from '../../src/components/Voting';
+
+describe('Voting', () => {
+
+	it('renders a pair of buttons', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]} />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons.length).to.equal(2);
+		expect(buttons[0].textContent).to.equal('Trainspotting');
+		expect(buttons[1].textContent).to.equal('28 Days Later');
+	});
+
+	it('invokes callback when a button is clicked', () => {
+		let votedWith;
+		const vote = (entry) => votedWith = entry;
+
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]}
+					vote={vote} />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(compnent, 'button');
+		Simulate.click(buttons[0]);
+
+		expect(votedWith).to.equal('Trainspotting');
+	});
+});
