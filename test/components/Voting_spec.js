6c87c793ff47f7ab2c636fde223ba09afbc8bf3a
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-25 10:55:58 -0600

    Add test suites for voting component

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -33,4 +33,26 @@ describe('Voting', () => {
 
 		expect(votedWith).to.equal('Trainspotting');
 	});
+
+	it('disables buttons when user has voted', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]}
+					hasVoted="Trainspotting" />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons.length).to.equal(2);
+		expect(buttons[0].textContent).to.equal('Trainspotting');
+		expect(buttons[1].textContent).to.equal('28 Days Later');
+	});
+
+	it('add label to the voted entry', () => {
+		const component = renderIntoDocument(
+			<Voting pair={["Trainspotting", "28 Days Later"]}
+					hasVoted="Trainspotting" />
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+		expect(buttons[0].textContent).to.contain('Voted');
+	});
 });
