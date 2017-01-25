518234912b1c1ac971d74cb311a46e2500bb3665
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-25 16:01:19 -0600

    Update app to calculate tally and add to Results component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {List} from 'immutable';
 import './App.css';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({'Trainspotting': 5, '28 Days Later': 4});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {
+      pair: pair,
+      tally: tally
+    });
   }
 });
--- src/components/Results.js
+++ src/components/Results.js
@@ -6,11 +6,20 @@ export default React.createClass({
 	getPair: function() {
     	return this.props.pair || [];
   	},
+  	getvotes: function(entry) {
+  		if (this.props.tally && this.props.tally.has(entry)) {
+  			return this.props.tally.get(entry);
+  		}
+  		return 0;
+  	}
 	render: function() {
 		return <div className="results">
 			{this.getPair().map(entry => {
 				<div key={entry} className="entry">
 					<h1>{entry}</h1>
+					<div className="voteCount">
+						{this.getVotes(entry)}
+					</div>
 				</div>
 			})}
 		</div>;
