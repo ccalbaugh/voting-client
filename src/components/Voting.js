fcefe5dbe92a54c4a5764600107c72303786db71
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-25 10:48:25 -0600

    Trim voting component and outsource Vote functionality to new component

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -1,29 +1,14 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default React.createClass({
-	getPair: function() {
-		return this.props.pair || [];
-	},
-	isDisabled: function() {
-		return !!this.props.hasVoted;
-	},
-	hasVotedFor: function(entry) {
-		return this.props.hasVoted === entry;
-	},
 	render: function() {
-		return <div className="voting">
+		return <div>
 			{this.props.winner ?
-				<div ref="winner"> Winner is {this.props.winner}!</div> :
-				this.getPair().map(entry =>
-					<button key={entry}
-							disabled={this.isDisabled()}
-							onclick={() => this.props.vote(entry)}>
-						<h1>{entry}</h1>
-						{this.hasVotedFor(entry) ?
-							<div className="label">Voted</div> :
-							null}
-					</button>
-			)}
+				<Winner ref="winner" winner={this.props.winner} /> :
+				<Vote {...this.props} />
+			}
 		</div>;
 	}
 });
